bd3fb8e6ca6ca1c4dc6b479509b173de3755683a
vinodhalaharvi <vinod.halaharvi@gmail.com>
2024-03-07 11:11:08 -0500

    Initial Commit

--- ginServer.go
+++ ginServer.go
@@ -14,7 +14,10 @@ func (s *GinServer) InstallCORS() {
 	s.Router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+		)
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
