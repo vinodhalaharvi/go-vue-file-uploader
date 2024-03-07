84d5cb45dd5e9d198932ef645d6b8237b44c212d
vinodhalaharvi <vinod.halaharvi@gmail.com>
2024-03-07 11:08:37 -0500

    Initial Commit

---
+++ ginServer.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+type GinServer struct {
+	Router *gin.Engine
+}
+
+// InstallCORS install CORS
+func (s *GinServer) InstallCORS() {
+	s.Router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+}
+
+// Ping ping handler
+func (s *GinServer) Ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+// Upload upload handler
+func (s *GinServer) Upload(c *gin.Context) {
+	file, _ := c.FormFile("file")
+	log.Printf("File: %v", file.Filename)
+	err := c.SaveUploadedFile(file, "static/"+file.Filename)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Error uploading file"})
+	}
+	c.JSON(200, gin.H{"message": "File uploaded successfully"})
+}
+
+// Run run server
+func (s *GinServer) Run() {
+	err := s.Router.Run(":8082")
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
+
+// InstallRoutes install routes
+func (s *GinServer) InstallRoutes() {
+	s.Router.GET("/api/ping", s.Ping)
+	s.Router.POST("/api/upload", s.Upload)
+}
--- main.go
+++ main.go
@@ -7,37 +7,16 @@ import (
 )
 
 func main() {
-	r := gin.Default()
+	server := GinServer{
+		Router: gin.Default(),
+	}
+	server.InstallCORS()
+	server.InstallRoutes()
+	server.Router.Static("/web", "./web/file-uploader/dist/")
 
-	// Serve static files from Vue.js 'dist' directory
-	r.Static("/", "./web/file-uploader/dist/")
-
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})
-
-	r.POST("/upload", func(c *gin.Context) {
-		file, _ := c.FormFile("file")
-		log.Printf("File: %v", file.Filename)
-		err := c.SaveUploadedFile(file, "static/"+file.Filename)
-		if err != nil {
-			c.JSON(400, gin.H{"message": "Error uploading file"})
-		}
-		c.JSON(200, gin.H{"message": "File uploaded successfully"})
-	})
-
-	fmt.Printf("http://localhost:8082\n")
-	err := r.Run(":8082")
+	fmt.Printf("http://localhost:8082/web/index.html\n")
+	err := server.Router.Run(":8082")
 	if err != nil {
-		log.Printf("Error: %v", err)
+		log.Fatalf("Error: %v", err)
 	}
 }
